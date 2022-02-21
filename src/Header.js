0ffdff18d9635b8b33c41ef08b6cadf1d01244fb
Md Anas Sabah <mdanassabah@gmail.com>
2022-02-21 21:58:55 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <nav className="header">
+      <img
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        className="header__logo"
+        alt="logo"
+      />
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDn0mdglnX91NrBceV39uY-thzBgizy18g",
+  authDomain: "clone-4a872.firebaseapp.com",
+  projectId: "clone-4a872",
+  storageBucket: "clone-4a872.appspot.com",
+  messagingSenderId: "808438727535",
+  appId: "1:808438727535:web:9d3a3eecf5d54f30411e9a",
+  measurementId: "G-THWPGCEPWN"
+};
